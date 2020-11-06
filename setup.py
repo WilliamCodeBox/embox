6e05e88b40c0a0d8f8e2a4c4f11a5d4770b97efa
WilliamCodeQuote <codequote@163.com>
2020-11-06 14:41:53 +0800

    add FDTD grid class

--- setup.py
+++ setup.py
@@ -21,7 +21,8 @@ LICENSE = "GNU General Public License v3 (GPLv3)"
 # Required packages for this module to be executed
 REQUIRED = [
     "numpy~=1.19.2",
-    "scipy~=1.5.2"
+    "scipy~=1.5.2",
+    "mpi4py~=3.0.3"
 ]
 
 # Optional packages
