d0b4eba6a6d0a14769240930ca490da6e0f56df2
WilliamCodeQuote <codequote@163.com>
2020-11-19 14:29:13 +0800

    travis got me

--- notes/mesh.py
+++ notes/mesh.py
@@ -1,4 +1,7 @@
 import pygmsh as msh
 
-mesh =  msh.occ.Geometry()
-
+def meshio_logo():
+    geo = msh.occ.Geometry()
+    container = geo.add_rectangle([0.0, 0.0, 0.0], 10.0, 10.0)
+    letter_i = geo.add_rectangle([2.0, 2.0, 0.0], 1.0, 4.5)
+    i_dot = geo.add_disk([2.5, 7.5, 0.0], 0.6)
